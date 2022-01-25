b7bc44985a80f3c6c3633ce707e8dd8da7f4dee9
Chintsai Hwo <jcglqmoyx@gmail.com>
2022-01-25 18:24:09 +0800

    modity check-in time

--- english_learning_task.py
+++ english_learning_task.py
@@ -105,7 +105,7 @@ def f():
         else:
             t = []
             for i in range(len(v) - 3, len(v)):
-                if '000000' <= v[i][1] <= '223000':
+                if '010000' <= v[i][1] <= '223000':
                     t.append(v[i][0])
             if len(t) >= 3:
                 records[handle] = t
